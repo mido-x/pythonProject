d204603c8657b74d1270e8340ca9eded1bb71986
mido-x <mido712@naver.com>
2023-04-04 20:51:56 +0900

    Initial commit

---
+++ test.py
@@ -0,0 +1,37 @@
+import os
+import random
+
+while True:
+    com=random.randint(1,3)
+    print("1. scissors")
+    print("2. rock")
+    print("3. paper")
+    me=int(input("code : "))
+    print("me : %d, com : %d"%(me,com))
+    if com==1:
+        if me==1:
+            print("draw")
+        elif me==2:
+            print("win")
+        elif me==3:
+            print("lose")
+        else:
+            print("Choose one out of 1 2 3")
+    elif com==2:
+        if me==1:
+            print("lose")
+        elif me==2:
+            print("draw")
+        elif me==3:
+            print("win")
+        else:
+            print("Choose one out of 1 2 3")
+    elif com==3:
+        if me==1:
+            print("win")
+        elif me==2:
+            print("lose")
+        elif me==3:
+            print("draw")
+        else:
+            print("Choose one out of 1 2 3")
